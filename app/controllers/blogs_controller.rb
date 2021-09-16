5e5659420692f4e8695b29b6525105aca3b43451
taku8a <takuya0505hsmt@gmail.com>
2021-09-16 11:32:00 +0000

    second commit

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,7 @@
 class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
+    
   end
 
   def show
@@ -10,7 +11,7 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
-  
+
   def create
     @blog = Blog.new(blog_params)
     if @blog.save
@@ -18,25 +19,25 @@ class BlogsController < ApplicationController
     else
       render :new
     end
-  
+
   end
 
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
   end
-  
+
   def destroy
-     blog = Blog.find(params[:id])
+    blog = Blog.find(params[:id])
     blog.destroy
     redirect_to blogs_path
   end
-  
+
    private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
